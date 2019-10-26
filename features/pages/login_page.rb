6a19b1a3ec900d19ec8e1f2074171b27c34e54bd
Gregor Gilchrist <gregorgilchrist@gmail.com>
2019-10-26 19:13:22 +0100

    added wrong email step

--- features/pages/login_page.rb
+++ features/pages/login_page.rb
@@ -10,4 +10,11 @@ class LoginPage
     @browser.navigate.to("https://login.sandbox.freeagent.com")
   end
 
+  def enterEmail(email)   
+    wait = Selenium::WebDriver::Wait.new(timeout: 10)
+    wait.until { @browser.find_element(id: "login_email") }
+    emailTextbox = @browser.find_element(id: "login_email")
+    emailTextbox.send_keys(email)
+  end
+
 end
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,11 +1,11 @@
 Given /^I navigate to the login page$/ do
-    loginPage = LoginPage.new(@browser)
-    loginPage.goToLoginPage()
-  sleep 3
+    @loginPage = LoginPage.new(@browser)
+    @loginPage.goToLoginPage()
 end
 
 When /^I login with an invalid credential combination$/ do
- 
+    @loginPage.enterEmail("wrongUser@email.com")
+    sleep 3
 end
 
 Then /^I should see the invalid credentials login error message$/ do
